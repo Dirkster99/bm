c835478734464d4c22fa95734840154a0d4da222
lycj <lycj@quickzip.org>
2014-11-14 15:23:34 +0800

    Added SuggestBox2. Modified Breadcrumb to support states.

--- BreadcrumbLib/Controls/Breadcrumb.xaml.cs
+++ BreadcrumbLib/Controls/Breadcrumb.xaml.cs
@@ -1,28 +1,242 @@
 ﻿namespace BreadcrumbLib.Controls
 {
-	using System.Windows.Controls;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
+    using BreadcrumbLib.Utils;
+    using System.Windows.Data;
+    using BreadcrumbLib.Interfaces;
+    using System.Threading;
 
-	/// <summary>
-	/// Display a ToggleButton and when it's clicked, show it's content as a dropdown.
-	/// </summary>
-	public class Breadcrumb : UserControl
-	{
-		#region constructors
-		/// <summary>
-		/// Static constructor
-		/// </summary>
-		static Breadcrumb()
-		{
-			DefaultStyleKeyProperty.OverrideMetadata(typeof(Breadcrumb),
-					new System.Windows.FrameworkPropertyMetadata(typeof(Breadcrumb)));
-		}
 
-		/// <summary>
-		/// Class constructor
-		/// </summary>
-		public Breadcrumb()
-		{
-		}
-		#endregion constructors
-	}
+    #region ToStringConverter
+    public class ToStringConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value == null ? "" : value.ToString();
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// Display a ToggleButton and when it's clicked, show it's content as a dropdown.
+    /// </summary>
+    public class Breadcrumb : UserControl
+    {
+        #region fields
+        public enum BreadcrumbStates { Refresh, NavigateTo, IsLoading, Updating }
+
+        public static readonly DependencyProperty SelectedValueProperty =
+          DependencyProperty.Register("SelectedValue", typeof(object),
+          typeof(Breadcrumb), new UIPropertyMetadata(null, OnSelectedValueChanged));
+
+        public static readonly DependencyProperty DisplayMemberPathProperty =
+            SuggestBox.SuggestBox2.DisplayMemberPathProperty.AddOwner(typeof(Breadcrumb));
+
+        public static readonly DependencyProperty ValuePathProperty =
+            SuggestBox.SuggestBox2.ValuePathProperty.AddOwner(typeof(Breadcrumb));
+
+        /// <summary>
+        /// For internal use only.
+        /// </summary>
+        public static readonly DependencyProperty SelectedValuePathProperty =
+          DependencyProperty.Register("SelectedValuePath", typeof(string),
+          typeof(Breadcrumb), new UIPropertyMetadata("", OnSelectedValueChanged));
+
+        public static readonly DependencyProperty PathConverterProperty =
+         DependencyProperty.Register("PathConverter", typeof(IValueConverter),
+         typeof(Breadcrumb), new UIPropertyMetadata(new ToStringConverter()));
+
+        public static readonly DependencyProperty PathSuggestSourceProperty =
+         DependencyProperty.Register("PathSuggestSource", typeof(ISuggestSource),
+         typeof(Breadcrumb), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty BreadcrumbStateProperty =
+                DependencyProperty.Register("BreadcrumbState", typeof(BreadcrumbStates),
+                typeof(Breadcrumb), new UIPropertyMetadata(BreadcrumbStates.Refresh));
+
+        public static readonly DependencyProperty IsLoadingProperty =
+                DependencyProperty.Register("IsLoading", typeof(bool),
+                typeof(Breadcrumb), new UIPropertyMetadata(false, new PropertyChangedCallback(OnStateChanged)));
+
+        public static readonly DependencyProperty IsUpdatingProperty =
+             DependencyProperty.Register("IsUpdating", typeof(bool),
+             typeof(Breadcrumb), new UIPropertyMetadata(false, new PropertyChangedCallback(OnStateChanged)));
+
+        public static readonly DependencyProperty IsEditingProperty =
+                DependencyProperty.Register("IsEditing", typeof(bool),
+                typeof(Breadcrumb), new UIPropertyMetadata(false, new PropertyChangedCallback(OnStateChanged)));
+
+        public static readonly DependencyProperty CancelCommandProperty =
+                       DependencyProperty.Register("CancelCommand", typeof(ICommand),
+                       typeof(Breadcrumb));
+
+        public static readonly DependencyProperty RefreshCommandProperty =
+                               DependencyProperty.Register("RefreshCommand", typeof(ICommand),
+                               typeof(Breadcrumb));
+
+        #endregion
+
+        #region constructors
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static Breadcrumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Breadcrumb),
+                    new System.Windows.FrameworkPropertyMetadata(typeof(Breadcrumb)));
+        }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public Breadcrumb()
+        {
+        }
+        #endregion constructors
+
+        #region methods
+
+        public static void OnStateChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            Breadcrumb b = (sender as Breadcrumb);
+            if (b.IsUpdating)
+                b.BreadcrumbState = BreadcrumbStates.Updating;
+            else
+                if (b.IsEditing)
+                    b.BreadcrumbState = BreadcrumbStates.NavigateTo;
+                else if (b.IsLoading)
+                    b.BreadcrumbState = BreadcrumbStates.IsLoading;
+                else b.BreadcrumbState = BreadcrumbStates.Refresh;
+        }
+
+        public static void OnSelectedValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            Breadcrumb b = (sender as Breadcrumb);
+            if (b.PathConverter != null && !b.IsUpdating)
+            {
+                b.IsUpdating = true;
+
+                if (b.IsEditing)
+                    b.SelectedValue = b.PathConverter.ConvertBack(b.SelectedValuePath, typeof(object), null, Thread.CurrentThread.CurrentCulture);
+                else
+                    b.SelectedValuePath = (string)b.PathConverter.Convert(b.SelectedValue, typeof(string), null, Thread.CurrentThread.CurrentCulture);
+
+                b.IsUpdating = false;
+            }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var toggle = this.Template.FindName("navtoggle", this) as ToggleButton;
+            var sbox = this.Template.FindName("sbox", this) as BreadcrumbLib.Controls.SuggestBox.SuggestBox2;
+
+            toggle.AddValueChanged(ToggleButton.IsCheckedProperty,
+               (o, e) =>
+               {
+                   if (IsEditing)
+                   {
+                       sbox.Focus();
+                       sbox.SelectAll();
+                   }
+               });
+            sbox.AddHandler(SuggestBox.SuggestBox2.ValueChangedEvent, (RoutedEventHandler)((o, e) =>
+            {
+                IsEditing = false;
+            }));
+            //sbox.AddHandler(TextBox.LostFocusEvent, (RoutedEventHandler)((o, e) =>
+            //{
+            //    IsEditing = false;
+            //}));
+        }
+
+        #endregion
+
+
+        #region DependencyProperties
+
+        public object SelectedValue
+        {
+            get { return GetValue(SelectedValueProperty); }
+            set { SetValue(SelectedValueProperty, value); }
+        }
+
+        internal string SelectedValuePath
+        {
+            get { return (string)GetValue(SelectedValuePathProperty); }
+            set { SetValue(SelectedValuePathProperty, value); }
+        }
+
+        public IValueConverter PathConverter
+        {
+            get { return (IValueConverter)GetValue(PathConverterProperty); }
+            set { SetValue(PathConverterProperty, value); }
+        }
+
+        public string DisplayMemberPath
+        {
+            get { return (string)GetValue(DisplayMemberPathProperty); }
+            set { this.SetValue(DisplayMemberPathProperty, value); }
+        }
+
+        public string ValuePath
+        {
+            get { return (string)GetValue(ValuePathProperty); }
+            set { this.SetValue(ValuePathProperty, value); }
+        }
+
+        public ISuggestSource PathSuggestSource
+        {
+            get { return (ISuggestSource)GetValue(PathSuggestSourceProperty); }
+            set { SetValue(PathSuggestSourceProperty, value); }
+        }
+
+        public BreadcrumbStates BreadcrumbState
+        {
+            get { return (BreadcrumbStates)GetValue(BreadcrumbStateProperty); }
+            set { this.SetValue(BreadcrumbStateProperty, value); }
+        }
+
+        public bool IsEditing
+        {
+            get { return (bool)GetValue(IsEditingProperty); }
+            set { this.SetValue(IsEditingProperty, value); }
+        }
+
+        public bool IsUpdating
+        {
+            get { return (bool)GetValue(IsUpdatingProperty); }
+            set { this.SetValue(IsUpdatingProperty, value); }
+        }
+
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { this.SetValue(IsLoadingProperty, value); }
+        }
+
+
+        public ICommand CancelCommand
+        {
+            get { return (ICommand)GetValue(CancelCommandProperty); }
+            set { this.SetValue(CancelCommandProperty, value); }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get { return (ICommand)GetValue(RefreshCommandProperty); }
+            set { this.SetValue(RefreshCommandProperty, value); }
+        }
+
+        #endregion
+    }
 }
---
+++ BreadcrumbLib/Controls/SuggestBox/SuggestBox2.xaml.cs
@@ -0,0 +1,354 @@
+﻿using BreadcrumbLib.Interfaces;
+using BreadcrumbLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace BreadcrumbLib.Controls.SuggestBox
+{
+    public class SuggestBox2 : TextBox
+    {
+        #region fields
+
+        private Popup _popup;
+        private ListBox _itemList;
+        private Grid _root;
+        private ScrollViewer _host;
+        private UIElement _textBoxView;
+        private bool _prevState;
+
+
+        public static readonly DependencyProperty DisplayMemberPathProperty = DependencyProperty.Register(
+            "DisplayMemberPath", typeof(string), typeof(SuggestBox2), new PropertyMetadata("Header"));
+
+        public static readonly DependencyProperty ValuePathProperty = DependencyProperty.Register(
+                "ValuePath", typeof(string), typeof(SuggestBox2), new PropertyMetadata("Value"));
+
+        public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent("ValueChanged",
+            RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(SuggestBox2));
+
+        public static readonly DependencyProperty SuggestionsProperty = DependencyProperty.Register(
+                "Suggestions", typeof(IList<object>), typeof(SuggestBox2), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty SuggestSourceProperty =
+        DependencyProperty.Register("SuggestSource", typeof(ISuggestSource),
+        typeof(SuggestBox2), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty IsPopupOpenedProperty =
+                DependencyProperty.Register("IsPopupOpened", typeof(bool),
+                typeof(SuggestBox2), new UIPropertyMetadata(false));
+
+        #endregion
+
+        #region constructors
+
+        static SuggestBox2()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SuggestBox2),
+                    new FrameworkPropertyMetadata(typeof(SuggestBox2)));
+        }
+
+        public SuggestBox2()
+        {
+        }
+
+        #endregion
+
+        #region events
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// This property are used internally to provide suggestions to PART_ItemList,
+        /// Suggestion are provided by SuggestSource.SuggestAsync()
+        /// </summary>
+        internal IList<object> Suggestions
+        {
+            get { return (IList<object>)GetValue(SuggestionsProperty); }
+            set { this.SetValue(SuggestionsProperty, value); }
+        }
+
+        /// <summary>
+        /// DisplayMemberPath of PART_ItemList, e.g. the text to display on suggestion.
+        /// </summary>
+        public string DisplayMemberPath
+        {
+            get { return (string)GetValue(DisplayMemberPathProperty); }
+            set { this.SetValue(DisplayMemberPathProperty, value); }
+        }
+
+        /// <summary>
+        /// ValuePath of PART_ItemList, e.g. the value on suggestion to be set to Text if doube-click or pressed enter.
+        /// </summary>
+        public string ValuePath
+        {
+            get { return (string)GetValue(ValuePathProperty); }
+            set { this.SetValue(ValuePathProperty, value); }
+        }
+
+        /// <summary>
+        /// Make suggestion object whenever Text changed and IsEnabled.
+        /// </summary>
+        public ISuggestSource SuggestSource
+        {
+            get { return (ISuggestSource)GetValue(SuggestSourceProperty); }
+            set { SetValue(SuggestSourceProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or Sets whether PART_Popup is opened.
+        /// </summary>
+        public bool IsPopupOpened
+        {
+            get { return (bool)GetValue(IsPopupOpenedProperty); }
+            set { this.SetValue(IsPopupOpenedProperty, value); }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Is called when a control template is applied.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this._popup = this.Template.FindName("PART_Popup", this) as Popup;
+            this._itemList = this.Template.FindName("PART_ItemList", this) as ListBox;
+            this._host = this.Template.FindName("PART_ContentHost", this) as ScrollViewer;
+            this._textBoxView = LogicalTreeHelper.GetChildren(this._host).OfType<UIElement>().First();
+            this._root = this.Template.FindName("root", this) as Grid;
+
+            Keyboard.Focus(this);
+            this.GotKeyboardFocus += (o, e) =>
+            {
+                this.popupIfSuggest();
+            };
+
+            this.LostKeyboardFocus += (o, e) =>
+            {
+                if (!IsKeyboardFocusWithin) this.hidePopup();
+            };
+
+            // 09-04-09 Based on SilverLaw's approach 
+            this._popup.CustomPopupPlacementCallback += new CustomPopupPlacementCallback(
+                    (popupSize, targetSize, offset) => new CustomPopupPlacement[] {
+                new CustomPopupPlacement(new Point((0.01 - offset.X),
+                    (this._root.ActualHeight - offset.Y)), PopupPrimaryAxis.None) });
+
+            #region _itemList event handlers - MouseDblClick, PreviewMouseUp, PreviewKeyDown
+            this._itemList.MouseDoubleClick += (o, e) =>
+            {
+                this.updateValueFromListBox();
+            };
+
+            this._itemList.PreviewMouseUp += (o, e) =>
+            {
+                if (this._itemList.SelectedValue != null)
+                    this.updateValueFromListBox();
+            };
+
+            this._itemList.PreviewKeyDown += (o, e) =>
+            {
+                if (e.OriginalSource is ListBoxItem)
+                {
+                    ListBoxItem lbItem = e.OriginalSource as ListBoxItem;
+
+                    e.Handled = true;
+                    switch (e.Key)
+                    {
+                        case Key.Enter:  // Handle in OnPreviewKeyDown
+                            break;
+
+                        case Key.Oem5:
+                            this.updateValueFromListBox(false);
+                            SetValue(TextProperty, Text + "\\");
+                            break;
+
+                        case Key.Escape:
+                            this.Focus();
+                            hidePopup();
+                            break;
+
+                        default:
+                            e.Handled = false; break;
+                    }
+
+                    if (e.Handled)
+                    {
+                        Keyboard.Focus(this);
+                        hidePopup();
+                        this.Select(Text.Length, 0); // Select last char
+                    }
+                }
+            };
+            #endregion
+
+            #region Hide popup when switch to another window
+            Window parentWindow = UITools.FindLogicalAncestor<Window>(this);
+
+            if (parentWindow != null)
+            {
+                parentWindow.Deactivated += delegate
+                {
+                    this._prevState = this.IsPopupOpened;
+                    this.IsPopupOpened = false;
+                };
+
+                parentWindow.Activated += delegate
+                {
+                    this.IsPopupOpened = this._prevState;
+                };
+            }
+            #endregion
+        }
+
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+
+            switch (e.Key)
+            {
+                case Key.Up:
+                case Key.Down:
+                case Key.Prior:
+                case Key.Next:
+                    if (this.Suggestions != null && this.Suggestions.Count > 0 && !(e.OriginalSource is ListBoxItem))
+                    {
+                        this.popupIfSuggest();
+                        this._itemList.Focus();
+                        this._itemList.SelectedIndex = 0;
+
+                        ListBoxItem lbi = this._itemList.ItemContainerGenerator.ContainerFromIndex(0) as ListBoxItem;
+
+                        if (lbi != null)
+                            lbi.Focus();
+
+                        e.Handled = true;
+                    }
+                    break;
+
+                case Key.Return:
+                    if (this._itemList.IsKeyboardFocusWithin)
+                        this.updateValueFromListBox();
+
+                    this.hidePopup();
+                    this.updateSource();
+
+                    e.Handled = true;
+                    break;
+
+                case Key.Back:
+                    if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                    {
+                        char separator = this.Text.Contains('/') ? '/' : this.Text.Contains("\\") ? '\\' : '!';
+
+                        if (this.Text.EndsWith("\\") || this.Text.EndsWith("/"))
+                            this.SetValue(SuggestBox2.TextProperty, this.Text.Substring(0, this.Text.Length - 1));
+                        else
+                        {
+                            int idx = Text.LastIndexOfAny(new char[] { '\\', '/' });
+                            if (idx != -1)
+                                this.SetValue(SuggestBox2.TextProperty, Text.Substring(0, idx));
+                        }
+
+                        this.Select(this.Text.Length, 0);
+                        e.Handled = true;
+                    }
+                    break;
+            }
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            string text = Text;
+
+            if (IsEnabled && SuggestSource != null)
+            {
+                var suggestSource = SuggestSource;
+                Task.Run(async () =>
+                {
+                    return await suggestSource.SuggestAsync(null, text, null);
+                }).ContinueWith((pTask) =>
+                {
+                    if (!pTask.IsFaulted)
+                    {
+                        this.SetValue(SuggestionsProperty, pTask.Result);
+                        this.popupIfSuggest();
+                    }
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+
+        //protected static void OnSuggestionsChanged(object sender, DependencyPropertyChangedEventArgs args)
+        //{
+        //    SuggestBox2 sbox = sender as SuggestBox2;
+        //    if (args.OldValue != args.NewValue)
+        //        sbox.popupIfSuggest();
+        //}
+
+        #region Helpers
+        /// <summary>
+        /// Show popup if there's suggestion.
+        /// </summary>
+        protected void popupIfSuggest()
+        {
+            if (this.IsFocused)
+                if (this.Suggestions != null && this.Suggestions.Count > 0)
+                    this.IsPopupOpened = true;
+                else
+                    this.IsPopupOpened = false;
+        }
+
+        /// <summary>
+        /// Hide popup.
+        /// </summary>
+        protected void hidePopup()
+        {
+            this.IsPopupOpened = false;
+        }
+
+        /// <summary>
+        /// Update ListBox.SelectedValue to Text
+        /// </summary>
+        /// <param name="updateSrc"></param>
+        private void updateValueFromListBox(bool updateSrc = true)
+        {
+            this.SetValue(TextBox.TextProperty, this._itemList.SelectedValue);
+
+            if (updateSrc)
+                this.updateSource();
+
+            this.hidePopup();
+        }
+
+        /// <summary>
+        /// Update source of TextBox.TextProperty expression 
+        /// </summary>
+        protected virtual void updateSource()
+        {
+            var txtBindingExpr = this.GetBindingExpression(TextBox.TextProperty);
+            if (txtBindingExpr == null)
+                return;
+
+            if (txtBindingExpr != null)
+                txtBindingExpr.UpdateSource();
+
+            this.RaiseEvent(new RoutedEventArgs(ValueChangedEvent));
+        }
+        #endregion
+        #endregion
+    }
+}
