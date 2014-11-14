6bc77d5dc3b3048cadc89afff03991b5058c0975
lycj <lycj@quickzip.org>
2014-11-14 15:17:40 +0800

    Added TreeRootSelectorViewModel.Refresh/CancelCommand. ITreeSelector.LookupAsync() now takes a CancellationToken.

--- Breadcrumb/ViewModels/Helpers/TreeRootSelectorViewModel.cs
+++ Breadcrumb/ViewModels/Helpers/TreeRootSelectorViewModel.cs
@@ -1,217 +1,255 @@
 ﻿namespace Breadcrumb.ViewModels.Helpers
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Collections.ObjectModel;
-	using System.Linq;
-	using System.Threading.Tasks;
-	using Breadcrumb.Defines;
-	using Breadcrumb.ViewModels.Interfaces;
-	using Breadcrumb.ViewModels.TreeLookupProcessors;
-	using Breadcrumb.ViewModels.TreeSelectors;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Breadcrumb.Defines;
+    using Breadcrumb.ViewModels.Interfaces;
+    using Breadcrumb.ViewModels.TreeLookupProcessors;
+    using Breadcrumb.ViewModels.TreeSelectors;
     using BreadcrumbLib.Utils;
+    using System.Threading;
+    using System.Windows.Input;
+    using System.Diagnostics;
 
-	public class TreeRootSelectorViewModel<VM, T> : TreeSelectorViewModel<VM, T>, ITreeRootSelector<VM, T>
-	{
-		#region fields
-		private T _selectedValue = default(T);
-		private ITreeSelector<VM, T> _selectedSelector;
-		private Stack<ITreeSelector<VM, T>> _prevPath = null;
-		private IEnumerable<ICompareHierarchy<T>> _comparers;
-		private ObservableCollection<VM> _rootItems = null;
-		#endregion fields
+    public class TreeRootSelectorViewModel<VM, T> : TreeSelectorViewModel<VM, T>, ITreeRootSelector<VM, T>
+    {
+        #region fields
+        private T _selectedValue = default(T);
+        private ITreeSelector<VM, T> _selectedSelector;
+        private Stack<ITreeSelector<VM, T>> _prevPath = null;
+        private IEnumerable<ICompareHierarchy<T>> _comparers;
+        private ObservableCollection<VM> _rootItems = null;
+        private CancellationTokenSource _cts = null;
+        public ICommand _refreshCommand, _cancelCommand;
 
-		#region constructors
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		/// <param name="entryHelper"></param>
-		/// <param name="compareFunc"></param>
-		/// <param name="rootLevel">Level of TreeItem to consider as root, root items should shown in expander 
-		/// (e.g. in OverflowedAndRootItems) and have caption and expander hidden when the path is longer than it.</param>
-		public TreeRootSelectorViewModel(IEntriesHelper<VM> entryHelper) ////int rootLevel = 0,
-			////params Func<T, T, HierarchicalResult>[] compareFuncs)
-			: base(entryHelper)
-		{
-			////_rootLevel = rootLevel;
-			////_compareFuncs = compareFuncs;
-			////Comparers = new [] { PathComparer.LocalDefault };
-		}
+        #endregion fields
 
-		#endregion constructors
+        #region constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="entryHelper"></param>
+        /// <param name="compareFunc"></param>
+        /// <param name="rootLevel">Level of TreeItem to consider as root, root items should shown in expander 
+        /// (e.g. in OverflowedAndRootItems) and have caption and expander hidden when the path is longer than it.</param>
+        public TreeRootSelectorViewModel(IEntriesHelper<VM> entryHelper) ////int rootLevel = 0,
+            ////params Func<T, T, HierarchicalResult>[] compareFuncs)
+            : base(entryHelper)
+        {
+            ////_rootLevel = rootLevel;
+            ////_compareFuncs = compareFuncs;
+            ////Comparers = new [] { PathComparer.LocalDefault };
+            RefreshCommand = new RelayCommand((sender) =>
+            {
+                Debug.WriteLine("Refresh");
+                this.SelectAsync(SelectedValue, true);
+            });
+            CancelCommand = new RelayCommand((sender) =>
+            {
+                Debug.WriteLine("Cancel");
+                CancellationTokenSource cts = _cts;
+                if (cts != null && !cts.IsCancellationRequested)
+                    cts.Cancel();
+            });
+        }
 
-		#region events
-		public event EventHandler SelectionChanged;
-		#endregion events
+        #endregion constructors
 
-		#region properties
-		public ObservableCollection<VM> OverflowedAndRootItems
-		{
-			get
-			{
-				if (this._rootItems == null)
-					this.updateRootItems();
+        #region events
+        public event EventHandler SelectionChanged;
+        #endregion events
 
-				return this._rootItems;
-			}
+        #region properties
 
-			set
-			{
-				this._rootItems = value;
-				this.NotifyOfPropertyChanged(() => this.OverflowedAndRootItems);
-			}
-		}
+        public ICommand RefreshCommand
+        {
+            get { return _refreshCommand; }
+            set { _refreshCommand = value; NotifyOfPropertyChanged(() => RefreshCommand); }
+        }
 
-		public ITreeSelector<VM, T> SelectedSelector
-		{
-			get { return this._selectedSelector; }
-		}
+        public ICommand CancelCommand
+        {
+            get { return _cancelCommand; }
+            set { _cancelCommand = value; NotifyOfPropertyChanged(() => CancelCommand); }
+        }
 
-		public VM SelectedViewModel
-		{
-			get
-			{
-				return (this.SelectedSelector == null ? default(VM) : this.SelectedSelector.ViewModel);
-			}
-		}
+        public ObservableCollection<VM> OverflowedAndRootItems
+        {
+            get
+            {
+                if (this._rootItems == null)
+                    this.updateRootItems();
 
-		/// <summary>
-		/// Gets/sets the select item when the user opens the drop down and selects 1 item
-		/// in the dropdownlist of the RootDropDown button.
-		/// </summary>
-		public T SelectedValue
-		{
-			get
-			{
-				return this._selectedValue;
-			}
+                return this._rootItems;
+            }
 
-			set
-			{
-				AsyncUtils.RunAsync(() => this.SelectAsync(value));
-			}
-		}
+            set
+            {
+                this._rootItems = value;
+                this.NotifyOfPropertyChanged(() => this.OverflowedAndRootItems);
+            }
+        }
 
-		////public int RootLevel { get { return _rootLevel; } set { _rootLevel = value; } }
+        public ITreeSelector<VM, T> SelectedSelector
+        {
+            get { return this._selectedSelector; }
+        }
 
-		public IEnumerable<ICompareHierarchy<T>> Comparers
-		{
-			get
-			{
-				return this._comparers;
-			}
-			
-			set
-			{
-				this._comparers = value;
-			}
-		}
-		#endregion properties
+        public VM SelectedViewModel
+        {
+            get
+            {
+                return (this.SelectedSelector == null ? default(VM) : this.SelectedSelector.ViewModel);
+            }
+        }
 
-		#region methods
-		public override void ReportChildSelected(Stack<ITreeSelector<VM, T>> path)
-		{
-			ITreeSelector<VM, T> prevSelector = this._selectedSelector;
 
-			T prevSelectedValue = this._selectedValue;
-			this._prevPath = path;
+        /// <summary>
+        /// Gets/sets the select item when the user opens the drop down and selects 1 item
+        /// in the dropdownlist of the RootDropDown button.
+        /// </summary>
+        public T SelectedValue
+        {
+            get
+            {
+                return this._selectedValue;
+            }
 
-			this._selectedSelector = path.Last();
-			this._selectedValue = path.Last().Value;
+            set
+            {
+                this.SelectAsync(value);
+            }
+        }
 
-			if (prevSelectedValue != null && !prevSelectedValue.Equals(path.Last().Value))
-			{
-				prevSelector.IsSelected = false;
-			}
+        ////public int RootLevel { get { return _rootLevel; } set { _rootLevel = value; } }
 
-			this.NotifyOfPropertyChanged(() => this.SelectedValue);
-			this.NotifyOfPropertyChanged(() => this.SelectedViewModel);
+        public IEnumerable<ICompareHierarchy<T>> Comparers
+        {
+            get
+            {
+                return this._comparers;
+            }
 
-			if (this.SelectionChanged != null)
-				this.SelectionChanged(this, EventArgs.Empty);
+            set
+            {
+                this._comparers = value;
+            }
+        }
+        #endregion properties
 
-			this.updateRootItems(path);
+        #region methods
+        public override void ReportChildSelected(Stack<ITreeSelector<VM, T>> path)
+        {
+            ITreeSelector<VM, T> prevSelector = this._selectedSelector;
+
+            T prevSelectedValue = this._selectedValue;
+            this._prevPath = path;
+
+            this._selectedSelector = path.Last();
+            this._selectedValue = path.Last().Value;
+
+            if (prevSelectedValue != null && !prevSelectedValue.Equals(path.Last().Value))
+            {
+                prevSelector.IsSelected = false;
+            }
+
+            this.NotifyOfPropertyChanged(() => this.SelectedValue);
+            this.NotifyOfPropertyChanged(() => this.SelectedViewModel);
+
+            if (this.SelectionChanged != null)
+                this.SelectionChanged(this, EventArgs.Empty);
+
+            this.updateRootItems(path);
 
             path.Last().EntryHelper.LoadAsync();
-		}
+        }
 
-		public override void ReportChildDeselected(Stack<ITreeSelector<VM, T>> path)
-		{
-		}
+        public override void ReportChildDeselected(Stack<ITreeSelector<VM, T>> path)
+        {
+        }
 
-		public async Task SelectAsync(T value)
-		{
-			if (this._selectedValue == null ||
-			    this.CompareHierarchy(this._selectedValue, value) != HierarchicalResult.Current)
-			{
-				await this.LookupAsync(value, RecrusiveSearch<VM, T>.LoadSubentriesIfNotLoaded,
-								SetSelected<VM, T>.WhenSelected, SetChildSelected<VM, T>.ToSelectedChild, 
-                                LoadSubEntries<VM,T>.WhenSelected(UpdateMode.Replace, false, null));
-			}
-		}
+        public async Task SelectAsync(T value, bool force = false)
+        {
+            if (force ||
+                this._selectedValue == null ||
+                this.CompareHierarchy(this._selectedValue, value) != HierarchicalResult.Current)
+            {
+                _cts = new CancellationTokenSource();
+                ITreeLookup<VM, T> lookupMethod = force ?
+                    RecrusiveSearch<VM, T>.AlwaysLoadSubEntries : RecrusiveSearch<VM, T>.LoadSubentriesIfNotLoaded;
 
-		public HierarchicalResult CompareHierarchy(T value1, T value2)
-		{
-			foreach (var c in this.Comparers)
-			{
-				var retVal = c.CompareHierarchy(value1, value2);
-				if (retVal != HierarchicalResult.Unrelated)
-					return retVal;
-			}
-			return HierarchicalResult.Unrelated;
-		}
+                await this.LookupAsync(value, lookupMethod, _cts.Token,
+                                SetSelected<VM, T>.WhenSelected, SetChildSelected<VM, T>.ToSelectedChild,
+                                LoadSubEntries<VM, T>.WhenSelected(UpdateMode.Replace, force, null));
+                _cts = null;
+            }
+        }
 
-		private async Task updateRootItemsAsync(ITreeSelector<VM, T> selector, ObservableCollection<VM> rootItems, int level)
-		{
-			if (level == 0)
-				return;
+        public HierarchicalResult CompareHierarchy(T value1, T value2)
+        {
+            foreach (var c in this.Comparers)
+            {
+                var retVal = c.CompareHierarchy(value1, value2);
+                if (retVal != HierarchicalResult.Unrelated)
+                    return retVal;
+            }
+            return HierarchicalResult.Unrelated;
+        }
 
-			List<ITreeSelector<VM, T>> rootTreeSelectors = new List<ITreeSelector<VM, T>>();
+        private async Task updateRootItemsAsync(ITreeSelector<VM, T> selector, ObservableCollection<VM> rootItems, int level)
+        {
+            if (level == 0)
+                return;
 
-			// Perform a lookup and for all directories in next level of current directory (load asynchronously if not loaded), 
-			// add directories's Selector to rootTreeSelectors.
-			await selector.LookupAsync(default(T),
-							BroadcastNextLevel<VM, T>.LoadSubentriesIfNotLoaded,
-							new TreeLookupProcessor<VM, T>(HierarchicalResult.All, (hr, p, c) =>
-											{
-												rootTreeSelectors.Add(c);
-												return true;
-											}));
+            List<ITreeSelector<VM, T>> rootTreeSelectors = new List<ITreeSelector<VM, T>>();
 
-			// Then foreach rootTreeSelectors, add to rootItems and preferm updateRootItemAsync.
-			foreach (var c in rootTreeSelectors)
-			{
-				rootItems.Add(c.ViewModel);
-				c.IsRoot = true;
-				
-				await this.updateRootItemsAsync(c, rootItems, level - 1);
-			}
-		}
+            // Perform a lookup and for all directories in next level of current directory (load asynchronously if not loaded), 
+            // add directories's Selector to rootTreeSelectors.
+            await selector.LookupAsync(default(T),
+                            BroadcastNextLevel<VM, T>.LoadSubentriesIfNotLoaded, CancellationToken.None,
+                            new TreeLookupProcessor<VM, T>(HierarchicalResult.All, (hr, p, c) =>
+                            {
+                                rootTreeSelectors.Add(c);
+                                return true;
+                            }));
 
-		/// <summary>
-		/// Method is executed when the user clicks the RootDropDown button.
-		/// </summary>
-		/// <param name="path"></param>
-		private void updateRootItems(Stack<ITreeSelector<VM, T>> path = null)
-		{
-			////if (_rootItems == null)
-			this._rootItems = new ObservableCollection<VM>();
-			////else _rootItems.Clear();
+            // Then foreach rootTreeSelectors, add to rootItems and preferm updateRootItemAsync.
+            foreach (var c in rootTreeSelectors)
+            {
+                rootItems.Add(c.ViewModel);
+                c.IsRoot = true;
 
-			if (path != null && path.Count() > 0)
-			{
-				foreach (var p in path.Reverse())
-				{
-					if (!(this.EntryHelper.AllNonBindable.Contains(p.ViewModel)))
-						this._rootItems.Add(p.ViewModel);
-				}
+                await this.updateRootItemsAsync(c, rootItems, level - 1);
+            }
+        }
 
-				this._rootItems.Add(default(VM)); // Separator
-			}
+        /// <summary>
+        /// Method is executed when the user clicks the RootDropDown button.
+        /// </summary>
+        /// <param name="path"></param>
+        private void updateRootItems(Stack<ITreeSelector<VM, T>> path = null)
+        {
+            ////if (_rootItems == null)
+            this._rootItems = new ObservableCollection<VM>();
+            ////else _rootItems.Clear();
 
-			// Get all items for display in the root drop down list
-			AsyncUtils.RunAsync(() => this.updateRootItemsAsync(this, this._rootItems, 1));
-		}
-		#endregion
-	}
+            if (path != null && path.Count() > 0)
+            {
+                foreach (var p in path.Reverse())
+                {
+                    if (!(this.EntryHelper.AllNonBindable.Contains(p.ViewModel)))
+                        this._rootItems.Add(p.ViewModel);
+                }
+
+                this._rootItems.Add(default(VM)); // Separator
+            }
+
+            // Get all items for display in the root drop down list
+            Task.Run(() => this.updateRootItemsAsync(this, this._rootItems, 1));
+        }
+        #endregion
+    }
 }
--- Breadcrumb/ViewModels/TreeSelectors/RecrusiveSearch.cs
+++ Breadcrumb/ViewModels/TreeSelectors/RecrusiveSearch.cs
@@ -1,77 +1,81 @@
 ﻿namespace Breadcrumb.ViewModels.TreeSelectors
 {
-	using System.Collections.Generic;
-	using System.Threading.Tasks;
-	using Breadcrumb.Defines;
-	using Breadcrumb.ViewModels.Interfaces;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Breadcrumb.Defines;
+    using Breadcrumb.ViewModels.Interfaces;
 
-	/// <summary>
-	/// Lookup until lookupvalue is found, and process only parent or matchednode.
-	/// </summary>
-	/// <typeparam name="VM"></typeparam>
-	/// <typeparam name="T"></typeparam>
-	public class RecrusiveSearch<VM, T> : ITreeLookup<VM, T>
-	{
-		#region fields
-		/// <summary>
-		/// Lookup until lookupvalue is found, and process only parent or matchednode.
-		/// Load subentries if not loaded.
-		/// </summary>
-		public static RecrusiveSearch<VM, T> LoadSubentriesIfNotLoaded = new RecrusiveSearch<VM, T>(true);
+    /// <summary>
+    /// Lookup until lookupvalue is found, and process only parent or matchednode.
+    /// </summary>
+    /// <typeparam name="VM"></typeparam>
+    /// <typeparam name="T"></typeparam>
+    public class RecrusiveSearch<VM, T> : ITreeLookup<VM, T>
+    {
+        #region fields
+        /// <summary>
+        /// Lookup until lookupvalue is found, and process only parent or matchednode.
+        /// Load subentries if not loaded.
+        /// </summary>
+        public static RecrusiveSearch<VM, T> LoadSubentriesIfNotLoaded = new RecrusiveSearch<VM, T>(true);
 
-		/// <summary>
-		/// Lookup until lookupvalue is found, and process only parent or matchednode.  Skip if not loaded.
-		/// </summary>
-		public static RecrusiveSearch<VM, T> SkipIfNotLoaded = new RecrusiveSearch<VM, T>(false);
+        public static RecrusiveSearch<VM, T> AlwaysLoadSubEntries = new RecrusiveSearch<VM, T>(true, true);
 
-		private bool _loadSubEntries;
-		#endregion fields
+        /// <summary>
+        /// Lookup until lookupvalue is found, and process only parent or matchednode.  Skip if not loaded.
+        /// </summary>
+        public static RecrusiveSearch<VM, T> SkipIfNotLoaded = new RecrusiveSearch<VM, T>(false);
 
-		#region constructors
-		/// <summary>
-		/// Class constructor
-		/// </summary>
-		/// <param name="loadSubEntries"></param>
-		public RecrusiveSearch(bool loadSubEntries)
-		{
-			this._loadSubEntries = loadSubEntries;
-		}
-		#endregion constructors
+        private bool _loadSubEntries;
+        private bool _force;
+        #endregion fields
 
-		public async Task LookupAsync(T value, ITreeSelector<VM, T> parentSelector,
-			 ICompareHierarchy<T> comparer, params ITreeLookupProcessor<VM, T>[] processors)
-		{
-			IEnumerable<VM> subentries = this._loadSubEntries ?
-					await parentSelector.EntryHelper.LoadAsync() :
-					 parentSelector.EntryHelper.AllNonBindable;
+        #region constructors
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="loadSubEntries"></param>
+        public RecrusiveSearch(bool loadSubEntries, bool force = false)
+        {
+            this._loadSubEntries = loadSubEntries;
+            this._force = force;
+        }
+        #endregion constructors
 
-			if (subentries != null)
-			{
-				foreach (VM current in subentries)
-				{
-					if (current is ISupportTreeSelector<VM, T> && current is ISupportEntriesHelper<VM>)
-					{
-						var currentSelectionHelper = (current as ISupportTreeSelector<VM, T>).Selection;
-						var compareResult = comparer.CompareHierarchy(currentSelectionHelper.Value, value);
-						switch (compareResult)
-						{
-							case HierarchicalResult.Current:
-								processors.Process(compareResult, parentSelector, currentSelectionHelper);
-								return;
+        public async Task LookupAsync(T value, ITreeSelector<VM, T> parentSelector,
+             ICompareHierarchy<T> comparer, params ITreeLookupProcessor<VM, T>[] processors)
+        {
+            IEnumerable<VM> subentries = this._loadSubEntries ?
+                    await parentSelector.EntryHelper.LoadAsync(UpdateMode.Update, _force) :
+                     parentSelector.EntryHelper.AllNonBindable;
 
-							case HierarchicalResult.Child:
-								if (processors.Process(compareResult, parentSelector, currentSelectionHelper))
-								{
-									await this.LookupAsync(value, currentSelectionHelper, comparer, processors);
+            if (subentries != null)
+            {
+                foreach (VM current in subentries)
+                {
+                    if (current is ISupportTreeSelector<VM, T> && current is ISupportEntriesHelper<VM>)
+                    {
+                        var currentSelectionHelper = (current as ISupportTreeSelector<VM, T>).Selection;
+                        var compareResult = comparer.CompareHierarchy(currentSelectionHelper.Value, value);
+                        switch (compareResult)
+                        {
+                            case HierarchicalResult.Current:
+                                processors.Process(compareResult, parentSelector, currentSelectionHelper);
+                                return;
 
-									return;
-								}
+                            case HierarchicalResult.Child:
+                                if (processors.Process(compareResult, parentSelector, currentSelectionHelper))
+                                {
+                                    await this.LookupAsync(value, currentSelectionHelper, comparer, processors);
 
-								break;
-						}
-					}
-				}
-			}
-		}
-	}
+                                    return;
+                                }
+
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
