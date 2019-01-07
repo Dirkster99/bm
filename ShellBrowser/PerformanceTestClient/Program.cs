aadbb14ac9c646bf11d4f26ae059100aa6ebce9b
Dirkster99 <Dirk.Bahle@gmx.de>
2019-01-07 18:36:17 +0100

    Small change in Enumeration Performace Test Client

--- ShellBrowser/PerformanceTestClient/Program.cs
+++ ShellBrowser/PerformanceTestClient/Program.cs
@@ -24,7 +24,8 @@
             Console.WriteLine("Retrieving all sub-directories from '{0}'...\n", dirPath);
 
             // List all known folders
-            Console.WriteLine("...{0} working on it...\n", DateTime.Now);
+            var startTime = DateTime.Now;
+            Console.WriteLine("...{0} working on it...\n", startTime);
 
             List<IDirectoryBrowser> result = new List<IDirectoryBrowser>();
             int i = 0;
@@ -33,13 +34,18 @@
                 result.Add(item);
                 i++;
 
-                if ((i % 1000) == 0)
-                    Console.Write("{0}...", i);
+                if ((i % 1000) == 0)        // print a little progress indicator
+                    Console.Write(".", i);
             }
 
             // List all known folders
-            Console.WriteLine("{0} Done retrieving {1} entries.\n", DateTime.Now, result.Count);
-            Console.ReadKey();
+            var endTime = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine("{0} Done retrieving {1} entries.\n", endTime, result.Count);
+            Console.WriteLine("After {0:n2} minutes or {1:n2} seconds.\n",
+                (endTime - startTime).TotalMinutes,
+                (endTime - startTime).TotalSeconds,
+                result.Count);
         }
     }
 }
